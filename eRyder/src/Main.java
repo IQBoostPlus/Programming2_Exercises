ff0456e55d1f963fb65b916a6ff369f04b70f9f6
IQBoostPlus <tacczy@outlook.com>
2026-03-13 11:05:18 +0800

    20260313

--- eRyder/src/Main.java
+++ eRyder/src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        String str1="I was very satisfied with the service.";
-        String str2="The e-Bike is quite comfortable to ride.";
-        String str3="The battery life of the e-Bike is impressive.";
-        String str4="The customer support was helpful and responsive.";
-        String str5="I would recommend this e-Bike to my friends and family.";
-        Feedback feedback1 = new Feedback("John", "Doe", "JohnDoe@gmail.com");
-        feedback1.analysizeFeedback(true, str1, str2, str3, str4, str5);
-        System.out.println(feedback1);
+        String commit1="I was very satisfied with the service.";
+        String commit2="The e-Bike is quite comfortable to ride.";
+        String commit3="The battery life of the e-Bike is impressive.";
+        String commit4="The customer support was helpful and responsive.";
+        String commit5="I would recommend this e-Bike to my friends and family.";
+        Feedback feedback = new Feedback("John", "Doe", "JohnDoe@gmail.com");
+        feedback.analysizeFeedback(true, commit1, commit2, commit3, commit4, commit5);
+        System.out.println(feedback);
     }
 }
